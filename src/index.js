3aef4c087dcb6f5158a56944fe947bd0676ac7b8
Andriy Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2023-06-19 22:54:53 +0300

    backend translation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import donent from 'dotenv'
 
-import authRoutes from './routes/auth.js'
+import { i18next, langMiddleware } from './i18next.js';
+import authRoutes from './routes/auth.js';
 
 const app = express()
 donent.config()
@@ -11,20 +12,28 @@ donent.config()
 //Middleware
 app.use(cors())
 app.use(express.json())
+app.use(i18next.init)
+app.use(langMiddleware)
 
 //Routes
-app.get('/test', (req, res) => { res.send(200) })
+app.get('/test/:len', (req, res) => {
+  res.send({ testTranslation: i18next.__('test') })
+})
 app.use('/api/auth', authRoutes)
 
 const start = () => {
-  mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-
-  app.listen(process.env.PORT, () => {
-    console.log(`server started at ${process.env.PORT} port`)
-  })
+  try {
+    mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+  
+    app.listen(process.env.PORT, () => {
+      console.log(`server started at ${process.env.PORT} port`)
+    })
+  } catch (error) {
+    console.log('start err', error)
+  }
 }
 
 start()
