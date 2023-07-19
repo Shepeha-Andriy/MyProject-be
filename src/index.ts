43d5787ac46e98f1b721e4dfc4060df677303ce5
Andriy Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2023-07-19 21:26:46 +0300

    file slice

--- src/index.ts
+++ src/index.ts
@@ -19,15 +19,31 @@ app.use(express.json())
 app.use(i18next.init)
 app.use(langMiddleware)
 app.use(helmet())
+app.use("/api/uploads/img", express.static("uploads/img"));
 
 //Routes
-app.get('/test', (req, res) => {
-  res.send({ testTranslation: i18next.__('test') })
-})
 app.use('/api/auth', authRoutes)
 app.use('/api/good', goodRoutes)
 app.use('/api/cart', cartRoutes)
 app.use('/api/order', orderRoutes)
+
+//Test
+import { buffer, compressAvatar } from './utils/uploadImg.js';
+import sharp from "sharp";
+import path from 'path';
+app.post("/test", buffer.array('image', 5), compressAvatar, async(req: any, res) => {
+  const absoluteAvatarsFolderPath = path.resolve("uploads", "img");
+  
+  for (const file of req.files) {
+    console.log(1)
+    console.log(file)
+    await sharp(file.buffer).toFile(
+      path.join(absoluteAvatarsFolderPath, file.originalname)
+    );
+  }
+
+  res.send({ testTranslation: i18next.__("test") });
+});
 
 const start = () => {
   try {
---
+++ src/utils/uploadImg.ts
@@ -0,0 +1,55 @@
+import multer from "multer";
+import sharp from "sharp";
+
+export const buffer = multer({
+  storage: multer.memoryStorage(),
+  fileFilter: (req, file, callback) => {
+    // console.log(file);
+    // console.log(1);
+    if (file.mimetype.startsWith("image")) {
+      callback(null, true);
+    } else {
+      callback(
+        new Error('err 224')
+      );
+    }
+  },
+});
+
+// export const uploadImg = (req, res) => {
+//   console.log(req.file)
+//   return res.json({
+//     url: `/uploads/img/${req.file.originalname}`,
+//   });
+// };
+
+export const compressAvatar = async ( req, res, next ) => {
+  if (!req.files) {
+    return next();
+  }
+  
+  try {
+    const resizedImages = [];
+
+    for (const file of req.files) {
+      const compressedImage = await sharp(file.buffer)
+        .resize({ width: 500 })
+        .jpeg({ quality: 50 })
+        .toBuffer();
+
+      file.buffer = compressedImage;
+      file.size = compressedImage.byteLength
+
+      if (file.buffer.byteLength > 500000) {
+        return next(new Error(`Maximum avatar size: ${500000 / 1000000} mb.`));
+      }
+
+      resizedImages.push(file);
+    }
+ 
+    req.files = resizedImages
+    next();
+  } catch (err) {
+    return next(err);
+  }
+};
